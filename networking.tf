028747118917600d24db6a63a84c9a5195c1c852
project <taitomgamer2409@gmail.com>
2025-11-10 15:39:05 +0700

    k

--- networking.tf
+++ networking.tf
@@ -13,3 +13,18 @@ resource "aws_subnet" "public" {
     Name = "demo-subnet"
   }
 }
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.demo_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "demo-subnet"
+  }
+}
