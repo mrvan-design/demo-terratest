af09b7821bd63c113919872c8ceff91c6d9605a1
project <taitomgamer2409@gmail.com>
2025-11-10 13:27:30 +0700

    thay doi provider ok

--- provider.tf
+++ provider.tf
@@ -17,8 +17,9 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    ec2 = "http://localhost:4566"
-    sts = "http://localhost:4566"
-    s3  = "http://localhost:4566"  # thêm dòng này cho S3
+ 
+    ec2 = "http://127.0.0.1:4566"
+    s3  = "http://127.0.0.1:4566"
+    sts = "http://127.0.0.1:4566" # thêm dòng này cho S3
   }
 }
