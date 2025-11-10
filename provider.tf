b854a7b24f62c26ec8375c2bc970e77ac3c38792
project <taitomgamer2409@gmail.com>
2025-11-10 11:30:42 +0700

    Cập nhật provider, variable và xóa file terraform cũ

--- provider.tf
+++ provider.tf
@@ -11,11 +11,13 @@ provider "aws" {
   region                      = var.aws_region
   access_key                  = "test"
   secret_key                  = "test"
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  s3_use_path_style           = true
+  skip_requesting_account_id  = true
+
   endpoints {
-    ec2 = "http://localhost:4566"
-    vpc = "http://localhost:4566"
+    ec2  = "http://localhost:4566"
+    sts  = "http://localhost:4566"
   }
 }
